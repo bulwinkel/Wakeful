a05cf38c6bbf1acb0fd00cf514d9f40733a07dd9
kelvin <kelvin@bulwinkel.com>
2016-05-28 20:22:15 +1000

    - added extra logging

--- app/src/main/java/com/bulwinkel/wakeful/WakefulTileService.kt
+++ app/src/main/java/com/bulwinkel/wakeful/WakefulTileService.kt
@@ -2,12 +2,15 @@ package com.bulwinkel.wakeful
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.IBinder
 import android.os.PowerManager
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import com.bulwinkel.tools.logd
 import com.bulwinkel.tools.loge
+import java.io.FileDescriptor
+import java.io.PrintWriter
 
 class WakefulTileService : TileService() {
 
@@ -27,11 +30,11 @@ class WakefulTileService : TileService() {
       if (wakelock.isHeld) {
         wakelock.release()
         tile.state = Tile.STATE_INACTIVE
-        logd { "wakelock released, state = ${qsTile.state}" }
+        logd { "wakelock released, state = ${tile.state}" }
       } else {
         wakelock.acquire()
         tile.state = Tile.STATE_ACTIVE
-        logd { "wakelock aquired, state = ${qsTile.state}" }
+        logd { "wakelock aquired, state = ${tile.state}" }
       }
       tile.updateTile()
     } else {
@@ -55,17 +58,57 @@ class WakefulTileService : TileService() {
   }
 
   override fun onBind(intent: Intent?): IBinder? {
-    logd { "onBind($intent)" }
+    logd { "onBind(intent: $intent, action: ${intent?.action}, extras: ${intent?.extras})" }
     return super.onBind(intent)
   }
 
   override fun onStopListening() {
-    super.onStopListening()
     logd { "onStopListening" }
+    super.onStopListening()
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration?) {
+    logd { "onConfigurationChanged(newConfig: $newConfig)" }
+    super.onConfigurationChanged(newConfig)
+  }
+
+  override fun onRebind(intent: Intent?) {
+    logd { "onRebind(intent: $intent, action: ${intent?.action}, extras: ${intent?.extras})" }
+    super.onRebind(intent)
+  }
+
+  override fun dump(fd: FileDescriptor?, writer: PrintWriter?, args: Array<out String>?) {
+    logd { "dump(fileDescriptor: $fd, writer: $writer, args: $args)" }
+    super.dump(fd, writer, args)
+  }
+
+  override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    logd { "onStartCommand(intent: $intent, action: ${intent?.action}, extras: ${intent?.extras}, flags: $flags, startId: $startId)" }
+    return super.onStartCommand(intent, flags, startId)
+  }
+
+  override fun onLowMemory() {
+    logd { "onLowMemory" }
+    super.onLowMemory()
+  }
+
+  override fun onTaskRemoved(intent: Intent?) {
+    logd { "onTaskRemoved(intent: $intent, action: ${intent?.action}, extras: ${intent?.extras})" }
+    super.onTaskRemoved(intent)
+  }
+
+  override fun onTrimMemory(level: Int) {
+    logd { "onTrimMemory(level: $level)" }
+    super.onTrimMemory(level)
+  }
+
+  override fun onUnbind(intent: Intent?): Boolean {
+    logd { "onUnbind(intent: $intent, action: ${intent?.action}, extras: ${intent?.extras})" }
+    return super.onUnbind(intent)
   }
 
   override fun onDestroy() {
-    super.onDestroy()
     logd { "onDestroy" }
+    super.onDestroy()
   }
 }
